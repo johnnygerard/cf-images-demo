4e95e2db0e0692a3c9a5b343d2924e14f7d6400b
Johnny Gérard <john@jgerard.dev>
2026-03-16 15:23:45 +0100

    revert: "build: use custom domain"
    This reverts commit 126fd344aedbb08762a6dd0e265711ee3f0b3791.

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
     },
   ],
   integrations: [sitemap()],
-  site: "https://cf-images-demo.jgerard.dev",
+  site: "https://cf-images-demo.mail-25a.workers.dev",
   trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
